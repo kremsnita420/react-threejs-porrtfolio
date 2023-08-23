e193fd69825776ab0fca076831666a047f5c5cae
Safet Duranovic <duranovic.safet@gmail.com>
2023-08-23 23:58:30 +0200

    Remove empty lines

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,7 +1,5 @@
 import { motion } from 'framer-motion';
-
 import { styles } from '../styles';
-
 import { SectionWrapper } from '../hoc';
 import { projects } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
