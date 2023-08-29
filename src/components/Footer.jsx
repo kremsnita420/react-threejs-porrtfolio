58c8eea1734abb0dea9b3dabd062ec879135a9aa
Safet Duranovic <duranovic.safet@gmail.com>
2023-08-29 12:25:48 +0200

    Adjust footer animation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,34 @@
+import { motion } from 'framer-motion';
 const name = 'Safet Duranovic';
 
 const Footer = () => {
 	return (
-		<footer className='flex flex-col items-center justify-center py-4 text-2xl text-center md:mt-8'>
+		<motion.footer className='flex flex-col items-center justify-center py-4 text-2xl text-center md:mt-8'>
 			made by{' '}
 			<a
 				className='flex px-4 py-3'
 				target='_blank'
 				rel='noreferrer'
 				href='https://www.linkedin.com/in/safet-duranovic/'>
-				{name.split('').map((n, i) => (
-					<span
+				{name.split('').map((letter, i) => (
+					<motion.span
+						animate={{
+							y: [0, 12, 0],
+						}}
+						transition={{
+							duration: 1.5,
+							repeat: Infinity,
+							delay: i * 0.33,
+							repeatType: 'loop',
+						}}
 						key={`letter-${i}`}
 						className={`block px-1 text-2xl md:text-4xl font-display`}>
-						{n}
-					</span>
+						{letter}
+					</motion.span>
 				))}
 			</a>{' '}
 			{new Date().getFullYear()}
-		</footer>
+		</motion.footer>
 	);
 };
 
